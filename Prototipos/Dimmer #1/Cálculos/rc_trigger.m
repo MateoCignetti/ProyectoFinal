1ac7e1884998a6a701d5d840d4bb41083e3e0ea5
MateoCignetti <mateocig18@gmail.com>
2025-02-25 01:23:19 +0100

    Primeros avances prototipo dimmer

---
+++ Prototipos/Dimmer #1/Cálculos/rc_trigger.m
@@ -0,0 +1,23 @@
+clear all, close all;
+
+R = 80e3;
+C = 470e-9;
+
+num = 1;
+den = [R*C 1];
+
+H = tf(num,den);
+
+figure();
+bode(H)
+
+t = [0:0.0001:160e-3];
+u = 220*sqrt(2)*sin(2*pi*50*t);
+
+[y, tOut] = lsim(H, u, t);
+
+figure();
+hold on;
+plot(tOut,y);
+plot(t, u);
+
