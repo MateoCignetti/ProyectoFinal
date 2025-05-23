966d26c3fc5ce611aff7039e049c58e597683783
MateoCignetti <mateocig18@gmail.com>
2025-05-23 01:05:52 -0300

    Agregar lectura de pote para tiempo de disparo de triac

--- Prototipos/Dimmer #1/control-dimmer/main/main.c
+++ Prototipos/Dimmer #1/control-dimmer/main/main.c
@@ -2,22 +2,29 @@
  * @file main.c
  * @author Mateo Antonio Cignetti (mateo@cignetti.ar)
  * @brief Isolated dimmer control for the dimmer prototype, using two gptimers to avoid blocking tasks
- * @version 0.9
+ * @version 1.0
  * @date 2025-05-25
  * 
  * @copyright Copyright (c) 2025
  * 
 */
 
+// IMPORTANT: The prototype's relay pin must be shorted to 3.3 V for the dimmer to be set in digital control mode.
+//            This was not included in this program to keep test components to a minimum.
+
 #include "driver/gpio.h" // GPIO driver
 #include "driver/gptimer.h" // Driver for the gptimers
 #include "esp_attr.h" // Needed for IRAM_ATTR (needed for interrupt handler)
 #include "esp_log.h" // ESP-IDF logging library
+#include "esp_adc/adc_oneshot.h" // ADC Oneshot driver
+#include "freertos/FreeRTOS.h" // FreeRTOS general library
+#include "freertos/task.h" // FreeRTOS task library
+
 
 // Generates a test 50 Hz square wave signal on the defined pin below,
 // to test the dimmer without the need of a real zero crossing detector.
 // Only for testing without the dimmer prototype connected.
-#define USE_TEST_ZCD_SIGNAL 1
+#define USE_TEST_ZCD_SIGNAL 0
 
 #if USE_TEST_ZCD_SIGNAL
 #include "driver/ledc.h"
@@ -26,16 +33,20 @@
 #endif
 //
 
-#define ESP_INTR_FLAG_DEFAULT 0 // Default interrupt flag
 
+#define ESP_INTR_FLAG_DEFAULT 0 // Default interrupt flag
 #define PIN_ZCD_IN GPIO_NUM_11 // Zero crossing detector input pin
 #define PIN_TRIAC_OUT GPIO_NUM_12 // TRIAC output pin
+#define ADC_READ_CHANNEL ADC_CHANNEL_2 // ADC channel to read (GPIO 3)
 #define PULSE_WIDTH_US 25 // Pulse width for driving the triac, in microseconds
 #define DIMMER_TIMER_COUNT_DEFAULT 9900 // Turns on TRIAC at 9,9 ms after zero crossing
                                         // (basically starts the dimmer off)
+#define ADC_SAMPLING_FREQUENCY 40 // ADC sampling frequency in Hz
 
-static gptimer_handle_t dimmer_wait_timer = NULL;
-static gptimer_handle_t dimmer_pulse_timer = NULL;
+static gptimer_handle_t dimmer_wait_timer = NULL; // Handle for the wait timer
+static gptimer_handle_t dimmer_pulse_timer = NULL; // Handle for the pulse timer
+static adc_oneshot_unit_handle_t adc1_handle = NULL; // Handle for the ADC1 unit
+static TaskHandle_t xTaskAdcRead_handle = NULL; // Task handle for the ADC read task
 
 static bool timer_is_running = false; // Aux flag to ensure proper timer operation, shouldn't be needed
                                       // under normal operation, but it's a safety measure (I guess?)
@@ -110,7 +121,7 @@ void configure_gpios(void){
 
     // Configuration for the TRIAC pin
     gpio_config_t triac_pin_conf = {
-        .pin_bit_mask = 1ULL << PIN_TRIAC_OUT,
+        .pin_bit_mask = 1ULL << PIN_TRIAC_OUT | 1ULL,
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_ENABLE,
@@ -189,6 +200,57 @@ void configure_test_signal(void){
 }
 #endif
 
+// Function to configure the ADC
+void configure_adc(){
+
+    // Configure the ADC1 unit
+    adc_oneshot_unit_init_cfg_t adc1_init_config = {
+        .unit_id = ADC_UNIT_1,
+        .clk_src = ADC_RTC_CLK_SRC_DEFAULT,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&adc1_init_config, &adc1_handle)); // Create the ADC handle
+
+    // Configure the ADC channel
+    adc_oneshot_chan_cfg_t adc1_channel_config = {
+        .atten = ADC_ATTEN_DB_12,
+        .bitwidth = ADC_BITWIDTH_12,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_READ_CHANNEL, &adc1_channel_config)); // Configure the ADC channel
+}
+
+// Function to map the ADC value to the timer alarm count
+float map(long x, long in_min, long in_max, long out_min, long out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// Task to read the ADC value and update the timer alarm count
+void vTaskAdcRead(void *pvParameters){
+    int adc_value = 0;
+    const int sampling_period = 1000 / ADC_SAMPLING_FREQUENCY; // Calculate the sampling period in milliseconds
+    TickType_t xLastWakeTime = xTaskGetTickCount(); // Get the current tick count
+
+    while(true){
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_READ_CHANNEL, &adc_value)); // Read the ADC value
+        ESP_LOGI("ADC", "ADC Value: %d", adc_value); // Print the ADC value
+        dimmer_wait_alarm_count = (uint16_t) map(adc_value, 0, 4095, 100, 9900); // Map the ADC value to the timer count
+        ESP_LOGI("ADC", "Dimmer alarm count: %d", dimmer_wait_alarm_count); // Print the ADC value
+
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(sampling_period)); // Delay the task for the sampling period
+    }
+}
+
+// Function to configure the tasks
+void configure_tasks(){
+    // Create the ADC read task
+    xTaskCreate(vTaskAdcRead,
+                "ADC Read Task",
+                5*2048,
+                NULL,
+                5,
+                &xTaskAdcRead_handle);
+}
+
 void app_main(void){
     
     // Configure the test signal if enabled
@@ -196,6 +258,8 @@ void app_main(void){
     configure_test_signal();
     #endif
     //
+    configure_adc(); // Configure the ADC
+    configure_tasks(); // Configure the tasks
 
     configure_timers(); // Configure the timers
     configure_gpios(); // Configure the GPIOs
